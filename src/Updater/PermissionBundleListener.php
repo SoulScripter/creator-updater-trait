c189b447027803a79a15972afbb6db788910bf52
denis <d.lissek@epubli.com>
2021-09-27 13:54:58 +0200

    update updater trait, add permissionBundleListener

---
+++ src/Updater/PermissionBundleListener.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SoulScripter\OrmTrait\Updater;
+
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Epubli\PermissionBundle\Service\AccessToken;
+
+class PermissionBundleListener
+{
+    private AccessToken $accessToken;
+
+    public function __construct(AccessToken $accessToken)
+    {
+        $this->accessToken = $accessToken;
+    }
+
+    public function preUpdate(LifecycleEventArgs $lifecycleEventArgs): void
+    {
+        $entity = $lifecycleEventArgs->getEntity();
+        if($entity->getUpdaterId() === null) {
+            $entity->setUpdaterId($this->accessToken->getUserId());
+        }
+    }
+}
