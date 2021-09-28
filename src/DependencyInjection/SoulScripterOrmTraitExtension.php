7edfa9add4a86dbe9696118b823c887a7265d069
denis <d.lissek@epubli.com>
2021-09-28 16:02:27 +0200

    fix spelling

--- src/DependencyInjection/SoulScripterOrmTraitExtension.php
+++ src/DependencyInjection/SoulScripterOrmTraitExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class SoulScripterOrmTraitExtension extends Extension
+class SoulScripterOrmTraitsExtension extends Extension
 {
     /**
      * @inheritDoc
