103b5f9864b75ff1ff94a6293ffd868957186fe9
denis <d.lissek@epubli.com>
2021-09-28 10:18:03 +0200

    initializing bundle

---
+++ src/OrmTraitBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SoulScripter\OrmTrait;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class OrmTraitBundle
+ * @package SoulScripter\OrmTrait
+ */
+class OrmTraitBundle extends Bundle
+{
+
+}
